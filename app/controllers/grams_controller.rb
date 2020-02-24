fd3748e3d11d3bb3f3cca7cb1e190f0c86b8df46
Dedrick Harrington <dedrick33@live.com>
2020-02-24 06:06:38 +0000

    Complete

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -56,7 +56,7 @@ class GramsController < ApplicationController
   private
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
 
 end
