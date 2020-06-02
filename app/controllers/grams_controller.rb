85faee308bf146223c326e46817ff1febf80fec1
Dedrick Harrington <dedrick33@live.com>
2020-06-02 03:14:55 +0000

    Grams controller

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -9,14 +9,7 @@ class GramsController < ApplicationController
     @grams = Gram.all
   end
 
-  def create
-    @gram = current_user.grams.create(gram_params)
-    if @gram.valid?
-      redirect_to root_path
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
+  
 
 
   def show
