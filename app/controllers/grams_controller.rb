5f23e7a0a8b7b4d0c82bb97bd650bd6ee1b89b2f
Dedrick Harrington <dedrick33@live.com>
2020-06-02 03:16:25 +0000

    Grams update

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -9,7 +9,14 @@ class GramsController < ApplicationController
     @grams = Gram.all
   end
 
-  
+  def create
+    @gram = current_user.grams.create(gram_params)
+    if @gram.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
 
   def show
